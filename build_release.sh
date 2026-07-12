8ee3e3dacc4a51920df1eb90f2a7e9c39b10677a
tommy <thomas.jurczyk@tjurczyk.com>
2026-07-12 15:21:23 +0200

    adding optimized sobel kernel

--- build_release.sh
+++ build_release.sh
@@ -9,7 +9,8 @@
 #   GAUSSIAN_OPT=2  pinned memory
 #
 #   SOBEL_OPT=0     naive         (default)
-#   SOBEL_OPT=1     optimized
+#   SOBEL_OPT=1     shared memory
+#   SOBEL_OPT=2     optimized version
 #
 #   NMS_OPT=0       naive         (default)
 #   NMS_OPT=1       shared memory
